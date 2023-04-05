88bbc327a23f4de032d0d07a98c7ff4f6a8ce1d9
Ian Rocha <iiaan.fr@gmail.com>
2023-04-05 20:19:03 -0300

    create user details component

---
+++ lib/features/get_user_info/ui/components/user_details_component.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:onboarding_challenge/shared/design_system/ds.dart';
+import 'package:onboarding_challenge/shared/strings/strings.dart';
+
+import '../../domain/entities/user.dart';
+
+class UserDetailsComponent extends StatelessWidget {
+  const UserDetailsComponent({super.key, required this.user});
+
+  final User user;
+
+  bool get hasAvatar => user.avatarUrl is String;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView(
+      padding: EdgeInsets.all(ds.spacing.small),
+      children: [
+        Row(
+          children: [
+            if (hasAvatar) ...[
+              CircleAvatar(
+                backgroundImage: NetworkImage(user.avatarUrl!),
+                radius: 40,
+              ),
+              SizedBox(width: ds.spacing.small)
+            ],
+            Expanded(
+              child: Text(
+                user.bio ?? '',
+                style: ds.typography.body1,
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: ds.spacing.medium),
+        Text(
+          strings.userDetailsScreen.repositoriesSubtitle,
+          style: ds.typography.subtitle,
+        ),
+        SizedBox(height: ds.spacing.small),
+        ...user.repositories
+            .map(
+              (repo) => Text(
+                '- ${repo.name}',
+                style: ds.typography.body2,
+              ),
+            )
+            .toList()
+      ],
+    );
+  }
+}
