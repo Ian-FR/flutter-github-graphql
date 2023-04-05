ab724746f727d04890752098da47c43ee4038cfd
Ian Rocha <iiaan.fr@gmail.com>
2023-04-05 20:15:58 -0300

    create app constants

---
+++ lib/core/constants.dart
@@ -0,0 +1,6 @@
+class AppConstants {
+  const AppConstants._();
+  static const appTitle = 'App titlte';
+  static const gitHubHostGrapQL = 'https://api.github.com/graphql';
+  static const gitHubPersonalToken = '<your_github_personal_token>';
+}
