2d6f056abf7ad2b7479ea4bf216a23ef78dac06a
Ian Rocha <iiaan.fr@gmail.com>
2023-04-05 20:20:10 -0300

    create a app strings object

---
+++ lib/shared/strings/strings.dart
@@ -0,0 +1,9 @@
+import 'package:onboarding_challenge/features/get_user_info/ui/strings/get_user_info_form_strings.dart';
+import 'package:onboarding_challenge/features/get_user_info/ui/strings/user_details_strings.dart';
+
+class Strings {
+  final getUserInfoForm = GetUserInfoFormStrings();
+  final userDetailsScreen = UserDetailsStrings();
+}
+
+final strings = Strings();
