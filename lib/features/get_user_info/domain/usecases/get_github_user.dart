2c9a78a0c9e2a4cd7cf1e15dbd027a59dd88a04a
Ian Rocha <iiaan.fr@gmail.com>
2023-04-05 20:17:46 -0300

    create GetGithubUser usecase

---
+++ lib/features/get_user_info/domain/usecases/get_github_user.dart
@@ -0,0 +1,38 @@
+import 'package:graphql/client.dart';
+import 'package:onboarding_challenge/shared/graph_ql/graph_ql.dart';
+
+import '../parsers/user_github_gql_parser.dart';
+import '../entities/user.dart';
+import '../errors/get_user_error.dart';
+
+class GetGithubUser {
+  Future<User> call(String login) async {
+    final result = await graphQL.client.value.query(
+      QueryOptions(
+        document: gql(_fetchGithubUserQuery),
+        variables: {
+          'login': login,
+          'nRepositories': 100,
+        },
+      ),
+    );
+
+    if (result.data == null) throw GetUserError.fetchingUser;
+
+    return UserGithubGqlParser.fromMap(result.data as Map);
+  }
+}
+
+const _fetchGithubUserQuery = '''
+  query FetchUser(\$login: String!, \$nRepositories: Int!) {
+    user(login: \$login) {
+      avatarUrl
+      bio
+      repositories(first: \$nRepositories) {
+        nodes {
+          name
+        }
+      }
+    }
+  }
+''';
