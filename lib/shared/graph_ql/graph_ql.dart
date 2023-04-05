8745cb26588150303c4ee4cbb797ebccbd4da537
Ian Rocha <iiaan.fr@gmail.com>
2023-04-05 20:12:17 -0300

    implements GraphQL client

---
+++ lib/shared/graph_ql/graph_ql.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:onboarding_challenge/core/constants.dart';
+
+class GraphQL {
+  late ValueNotifier<GraphQLClient> client;
+
+  init() async {
+    await initHiveForFlutter();
+
+    final httpLink = HttpLink(AppConstants.gitHubHostGrapQL);
+
+    final authLink = AuthLink(getToken: () => 'Bearer ${AppConstants.gitHubPersonalToken}');
+
+    final link = authLink.concat(httpLink);
+
+    client = ValueNotifier(
+      GraphQLClient(
+        link: link,
+        cache: GraphQLCache(store: HiveStore()),
+      ),
+    );
+  }
+}
+
+final graphQL = GraphQL();
